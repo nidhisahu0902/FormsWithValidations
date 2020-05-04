2720c1fddb1207a9e87e94f73a663de872d2bbd5
Nidhi Sahu <nidhinids0902@gmail.com>
2020-05-04 14:10:51 +0530

    Validation Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styles: ['input.ng-invalid{border-left:5px solid red;} input.ng-valid{border-left:5px solid green;}']
+})
+export class AppComponent {
+  title = 'form2';
+  myName = ""
+  
+
+  onSubmit(value: any)
+  {
+      console.log(value);
+  }
+}
